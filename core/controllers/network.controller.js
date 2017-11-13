6834bb5bcad5094554054188f6bde2568648ce0e
Welkin Wong <Welkin@Welkins-MacBook.local>
2017-11-14 00:47:47 +0800

    update

--- core/controllers/network.controller.js
+++ core/controllers/network.controller.js
@@ -16,7 +16,7 @@ exports.list = function (req, res) {
     }
 
     var networkSource = stdout.toString().split(/\n\n/);
-
+console.log(networkSource);
     networkSource = _.map(networkSource, function (item) {
       var data = {};
 
@@ -28,9 +28,9 @@ exports.list = function (req, res) {
 
       return data;
     });
-console.log(networkSource);
+
     networkSource = _.reject(networkSource, { name: 'lo' });
-console.log(networkSource);
+
     exec('ifconfig', function (err, stdout) {
       if (err) {
         logger.system().error(__filename, '打印网卡信息失败', err);
