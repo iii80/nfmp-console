6f13a2a3ee4b7a3332d4232db8ff10e46d32d041
Welkin Wong <Welkin@Welkins-MacBook.local>
2017-11-18 04:04:52 +0800

    update

--- core/services/stream.service.js
+++ core/services/stream.service.js
@@ -62,7 +62,7 @@ function writePid (id, pid, callback) {
  * 运行命令
  * @callback {Object} 网卡列表
  */
-exports.runCMD = function (id, cmd, callback) {
+exports.runCMD = function (id, cmd) {
   function startServer() {
     server = spawn(cmd[0], cmd[1]);
 
@@ -99,7 +99,5 @@ exports.runCMD = function (id, cmd, callback) {
 
       restart(signal);
     });
-
-    callback(server.pid);
   }; startServer();
 };
