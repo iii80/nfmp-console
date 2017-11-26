c57e41ad219bb6fd83d9ee899df2910c43e74971
Welkin Wong <welkin.wong@me.com>
2017-11-26 17:51:00 +0800

    update

--- core/controllers/streams.controller.js
+++ core/controllers/streams.controller.js
@@ -251,9 +251,9 @@ exports.create = function (req, res) {
       var normal = [];
 
       if (stream.inNetwork) {
-        normal = ['-i', '"' + stream.url + '?source=' + stream.inNetwork  + '"'];
+        normal = ['-stimeout', '5000000', '-i', '"' + stream.url + '?source=' + stream.inNetwork  + '"'];
       } else {
-        normal = ['-i', '"' + stream.url + '"'];
+        normal = ['-stimeout', '5000000', '-i', '"' + stream.url + '"'];
       }
 
       var cmd = [];
