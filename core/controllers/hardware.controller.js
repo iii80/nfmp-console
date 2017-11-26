29eeef16bfd8b85be5c6a1844418d7aca1e5ada9
Welkin Wong <welkin.wong@me.com>
2017-11-26 16:06:40 +0800

    update

--- core/controllers/hardware.controller.js
+++ core/controllers/hardware.controller.js
@@ -8,7 +8,114 @@ var spawn = require('child_process').spawn;
 var logger = require('../../lib/logger.lib');
 
 var oldListData = [];
-var currData = [];
+var network = [];
+var cpu = {};
+var mem = {};
+var network = {};
+
+/**
+ * 获取硬件信息
+ */
+setInterval(function () {
+  /**
+   * 获取网卡信息
+   */
+  var sourceData =  fs.readFileSync('/proc/net/dev').toString();
+
+  // 按换行符分割数据转数组
+  var clearN = sourceData.split('\n');
+
+  // 去掉表头和空格
+  var clearHead = _(clearN).drop(2).initial().value();
+
+  // 转为格式化数据
+  var list = _.map(clearHead, function (item) {
+    // 按空格分割数据转数组
+    var clearSpace = item.split(' ');
+
+    // 过滤掉空格
+    return _.filter(clearSpace, function (item) {
+      return item !== '';
+    });
+  });
+
+  function totalData (type, data) {
+    return _.reduce(data, function(total, n) {
+      if (type === 'receive') {
+        return total + Number(n[1]);
+      } else if (type === 'transmit') {
+        return total + Number(n[9]);
+      }
+    }, 0);
+  }
+
+  if (network.length === 0) {
+    network = _.map(list, function (item) {
+      return { name: item[0], receive: 0, transmit: 0 };
+    });
+
+    network.unshift({ name: '全部网卡', receive: 0, transmit: 0 });
+  } else {
+    network = _.map(list, function (item, index) {
+      return {
+        name: '网卡' + item[0].replace(/:/, ''),
+        receive:  _.floor((Number(item[1]) - Number(oldListData[index][1])) * 8 / 1024),
+        transmit:  _.floor((Number(item[9]) - Number(oldListData[index][9])) * 8 / 1024)
+      };
+    });
+
+    network.unshift({
+      name: '全部网卡',
+      receive: _.floor((totalData('receive', list) - totalData('receive', oldListData)) * 8 / 1024),
+      transmit: _.floor((totalData('transmit', list) - totalData('transmit', oldListData)) * 8 / 1024)
+    });
+  }
+
+  oldListData = list;
+
+  /**
+   * 获取CPU信息
+   */
+  osUtils.cpuUsage(function (cpuUsage) {
+    var osCpu = os.cpus();
+
+    cpu = {
+      usage: cpuUsage * 100,
+      model: osCpu[0].model,
+      amount: osCpu.length
+    };
+  });
+
+  /**
+   * 获取内存信息
+   */
+  var prc = spawn('free', []);
+  prc.stdout.setEncoding('utf8');
+  prc.stdout.on('data', function (data) {
+    var lines = data.toString().split(/\n/g);
+    var line = lines[1].split(/\s+/);
+    var total = parseInt(line[1], 10);
+    // var free = parseInt(line[3], 10);
+    // var buffers = parseInt(line[5], 10);
+    // var cached = parseInt(line[6], 10);
+    // var actualFree = free + buffers + cached;
+
+    mem = {
+      total: total,
+      usage: parseInt(line[2], 10)
+      // free: free,
+      // shared: parseInt(line[4], 10),
+      // buffers: buffers,
+      // cached: cached,
+      // actualFree: actualFree,
+      // percentUsed: parseFloat(((1 - (actualFree / total)) * 100).toFixed(2)),
+      // comparePercentUsed: ((1 - (os.freemem() / os.totalmem())) * 100).toFixed(2)
+    };
+  });
+  prc.on('error', function (err) {
+    logger.system().error(__filename, '获取内存错误', err);
+  });
+}, 100);
 
 /**
  * 硬件信息
@@ -16,122 +123,11 @@ var currData = [];
  * @param {Object} res
  */
 exports.information = function (socket) {
-  setInterval(function () {
-    /**
-     * 获取网卡信息
-     */
-    var sourceData =  fs.readFileSync('/proc/net/dev').toString();
+  var output = {
+    cpu: cpu,
+    mem: mem,
+    network: network
+  };
 
-    // 按换行符分割数据转数组
-    var clearN = sourceData.split('\n');
-
-    // 去掉表头和空格
-    var clearHead = _(clearN).drop(2).initial().value();
-
-    // 转为格式化数据
-    var list = _.map(clearHead, function (item) {
-      // 按空格分割数据转数组
-      var clearSpace = item.split(' ');
-
-      // 过滤掉空格
-      return _.filter(clearSpace, function (item) {
-        return item !== '';
-      });
-    });
-
-    function totalData (type, data) {
-      return _.reduce(data, function(total, n) {
-        if (type === 'receive') {
-          return total + Number(n[1]);
-        } else if (type === 'transmit') {
-          return total + Number(n[9]);
-        }
-      }, 0);
-    }
-
-    if (currData.length === 0) {
-      currData = _.map(list, function (item) {
-        return { name: item[0], receive: 0, transmit: 0 };
-      });
-
-      currData.unshift({ name: '全部网卡', receive: 0, transmit: 0 });
-    } else {
-      currData = _.map(list, function (item, index) {
-        return {
-          name: '网卡' + item[0].replace(/:/, ''),
-          receive:  _.floor((Number(item[1]) - Number(oldListData[index][1])) * 8 / 1024),
-          transmit:  _.floor((Number(item[9]) - Number(oldListData[index][9])) * 8 / 1024)
-        };
-      });
-
-      currData.unshift({
-        name: '全部网卡',
-        receive: _.floor((totalData('receive', list) - totalData('receive', oldListData)) * 8 / 1024),
-        transmit: _.floor((totalData('transmit', list) - totalData('transmit', oldListData)) * 8 / 1024)
-      });
-    }
-
-    oldListData = list;
-
-    async.parallel({
-      cpu: function (callback) {
-        osUtils.cpuUsage(function (cpuUsage) {
-          var osCpu = os.cpus();
-
-          var cpu = {
-            usage: cpuUsage * 100,
-              model: osCpu[0].model,
-              amount: osCpu.length
-          };
-
-          callback(null, cpu);
-        });
-      },
-      mem: function (callback) {
-        var prc = spawn('free', []);
-        prc.stdout.setEncoding('utf8');
-        prc.stdout.on('data', function (data) {
-          var lines = data.toString().split(/\n/g);
-          var line = lines[1].split(/\s+/);
-          var total = parseInt(line[1], 10);
-          // var free = parseInt(line[3], 10);
-          // var buffers = parseInt(line[5], 10);
-          // var cached = parseInt(line[6], 10);
-          // var actualFree = free + buffers + cached;
-
-          var mem = {
-            total: total,
-            usage: parseInt(line[2], 10)
-            // free: free,
-            // shared: parseInt(line[4], 10),
-            // buffers: buffers,
-            // cached: cached,
-            // actualFree: actualFree,
-            // percentUsed: parseFloat(((1 - (actualFree / total)) * 100).toFixed(2)),
-            // comparePercentUsed: ((1 - (os.freemem() / os.totalmem())) * 100).toFixed(2)
-          };
-
-          callback(null, mem);
-        });
-
-        prc.on('error', function (err) {
-          logger.system().error(__filename, '获取内存错误', err);
-        });
-      }
-    }, function (err, results) {
-      if (err) {
-        logger[err.type]().error(__filename, '获取硬件信息失败', err);
-
-        return false;
-      }
-
-      var output = {
-        cpu: results.cpu,
-        mem: results.mem,
-        network: currData
-      };
-
-      socket.emit('hardware', output);
-    });
-  }, 100);
+  socket.emit('hardware', output);
 };
