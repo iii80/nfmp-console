db2c4e439954ac9ce7b0886d4a5a5957a868404c
Welkin Wong <welkin.wong@me.com>
2018-01-11 01:52:58 +0800

    update

--- core/services/stream.service.js
+++ core/services/stream.service.js
@@ -64,6 +64,7 @@ exports.runCMD = function (id, cmd, callback) {
 
   function startServer() {
     server = spawn(cmd[0], cmd[1]);
+    console.log(server.spawnargs);
     console.log(server);
     writePid(id, server.pid, callback);
 
