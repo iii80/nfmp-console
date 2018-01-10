a4770081a6efc0496f8d77fcc5bdf2523528818b
Welkin Wong <welkin.wong@me.com>
2018-01-11 02:06:21 +0800

    update

--- core/services/stream.service.js
+++ core/services/stream.service.js
@@ -65,7 +65,6 @@ exports.runCMD = function (id, cmd, callback) {
   function startServer() {
     server = spawn(cmd[0], cmd[1]);
     console.log(server.spawnargs);
-    console.log(server);
     writePid(id, server.pid, callback);
 
     function restart() {
